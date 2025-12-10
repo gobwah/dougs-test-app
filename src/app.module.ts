751c009b57c65663ca12dad0da6d70e95e4ea11a
gobwah <vincou40@gmail.com>
2025-12-10 14:08:03 +0100

    Refactor health controller import path and remove outdated file
    - Updated the import path for `HealthController` to reflect the new directory structure.
    - Deleted the outdated `health.controller.ts` file to streamline the codebase.
    
    These changes enhance code organization and maintainability within the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BalanceService } from './models/balances/balance.service';
 import { DuplicateService } from './models/duplicates/duplicate.service';
-import { HealthController } from './models/health/health.controller';
+import { HealthController } from './health/health.controller';
 import { MovementController } from './models/movements/movement.controller';
 import { MovementService } from './models/movements/movement.service';
 
